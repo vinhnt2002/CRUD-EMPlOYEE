88d21c43f8df16025abfbe5d274a64b1340656da
vinh nguyễn <vinhntse2002@gmail.com>
2023-06-14 15:04:23 +0700

    finish

--- react-crud-employees-example/src/context/AppReducer.js
+++ react-crud-employees-example/src/context/AppReducer.js
@@ -1,44 +1,35 @@
-
-
-// This is CRUD action
-
-const appReducer = (state, action) => {
-
+export default function appReducer(state, action) {
     switch (action.type) {
-        case "ADD_EMPLOYEE":
-            return {
-                ...state,
-                employees: [...state.employees, action.payload],
-            }
-
-        case "EDIT_EMPLYEE":
-            const updatedEmployee = action.payload
-
-            const updatedEmployees = state.employees.map((employee) => {
-                if (employee.id === updatedEmployee.id)
-
-                    return updatedEmployee;
-
-                //else
-                return employee;
-
-            })
-            return {
-                ...state,
-                employees: updatedEmployees
-            }
-
-        case "REMOVE_EMPLOYEE":
-            return {
-                ...state,
-                employee: state.employees.filter(
-                    (employee) => employee.id !== action.payload
-                )
-            }
-        default:
-            return state;
+      case "ADD_EMPLOYEE":
+        return {
+          ...state,
+          employees: [...state.employees, action.payload],
+        };
+  
+      case "EDIT_EMPLOYEE":
+        const updatedEmployee = action.payload;
+  
+        const updatedEmployees = state.employees.map((employee) => {
+          if (employee.id === updatedEmployee.id) {
+            return updatedEmployee;
+          }
+          return employee;
+        });
+  
+        return {
+          ...state,
+          employees: updatedEmployees,
+        };
+  
+      case "REMOVE_EMPLOYEE":
+        return {
+          ...state,
+          employees: state.employees.filter(
+            (employee) => employee.id !== action.payload
+          ),
+        };
+  
+      default:
+        return state;
     }
-
-};
-
-export default appReducer;
+  };
--- react-crud-employees-example/src/index.js
+++ react-crud-employees-example/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import './tailwind.css';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+import './tailwind.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
